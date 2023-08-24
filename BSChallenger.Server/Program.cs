7a5c974a253558a75db4a30001236d74dd36856f
Futuremappermydud <54294576+Futuremappermydud@users.noreply.github.com>
2023-08-24 16:14:45 -0400

    feat: Rate Limiting

--- BSChallenger.Server/Program.cs
+++ BSChallenger.Server/Program.cs
@@ -1,68 +1,77 @@
 using BSChallenger.Server.Configuration;
-using BSChallenger.Server.Models;
+using BSChallenger.Server.Discord;
 using BSChallenger.Server.Extensions;
+using BSChallenger.Server.Filters;
+using BSChallenger.Server.Models;
+using Discord.Interactions;
+using Discord.WebSocket;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.RateLimiting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using BSChallenger.Server.Filters;
 using Microsoft.OpenApi.Models;
-using BSChallenger.Server.Discord;
-using System.Threading;
 using System;
-using Discord.Interactions;
-using Discord.WebSocket;
+using System.Threading.RateLimiting;
 
 namespace BSChallenger.Server
 {
-    public static class Program
-    {
-        public static Version Version = new Version(1, 0, 0);
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+	public static class Program
+	{
+		public static Version Version = new Version(1, 0, 0);
+		public static void Main(string[] args)
+		{
+			CreateHostBuilder(args).Build().Run();
+		}
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                    webBuilder
-                        .ConfigureServices((hostBuilderContext, services) =>
-                        {
-                            services.AddScoped<SecretProvider, SecretProvider>();
-                            services
-							    .AddSingleton<DiscordSocketClient>()
-							    .AddSingleton<InteractionService>()
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+					webBuilder
+						.ConfigureServices((hostBuilderContext, services) =>
+						{
+							services.AddScoped<SecretProvider, SecretProvider>();
+							services
+								.AddRateLimiter(_ => _
+									.AddSlidingWindowLimiter("slidingPolicy", options =>
+									{
+										options.PermitLimit = 60;
+										options.Window = TimeSpan.FromSeconds(10);
+										options.SegmentsPerWindow = 4;
+										options.QueueProcessingOrder = QueueProcessingOrder.OldestFirst;
+										options.QueueLimit = 5;
+									}))
+								.AddSingleton<DiscordSocketClient>()
+								.AddSingleton<InteractionService>()
 								.AddHostedService<DiscordBot>()
-                                .AddOptions()
-                                .AddConfiguration<AppConfiguration>("App")
-                                .AddDbContext<Database>()
-                                .AddSingleton<BeatleaderAPI>()
-                                .AddSingleton<BPListParser>()
-                                .AddSingleton<TokenProvider>()
-                                .AddSingleton<PasswordProvider>()
-                                .AddSwaggerGen(c =>
-                                {
-                                    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Challenger API", Version = "v1" });
-                                })
-                                .AddControllers(options =>
-                                    options.Filters.Add(new HttpResponseExceptionFilter())
-                                );
-                        })
-                        .Configure(applicationBuilder =>
-                            applicationBuilder
-                                .UseSwagger()
-                                .UseSwaggerUI(c =>
-                                {
-                                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Challenger API V1");
-                                })
-                                .UseRouting()
-                                .UseEndpoints(endPointRouteBuilder => endPointRouteBuilder.MapControllers())
-                        )
-                )
-                .UseSerilog();
-    }
+								.AddOptions()
+								.AddConfiguration<AppConfiguration>("App")
+								.AddDbContext<Database>()
+								.AddSingleton<BeatleaderAPI>()
+								.AddSingleton<BPListParser>()
+								.AddSingleton<TokenProvider>()
+								.AddSingleton<PasswordProvider>()
+								.AddSwaggerGen(c =>
+								{
+									c.SwaggerDoc("v1", new OpenApiInfo { Title = "Challenger API", Version = "v1" });
+								})
+								.AddControllers(options =>
+									options.Filters.Add(new HttpResponseExceptionFilter())
+								);
+						})
+						.Configure(applicationBuilder =>
+							applicationBuilder
+								.UseRateLimiter()
+								.UseSwagger()
+								.UseSwaggerUI(c =>
+								{
+									c.SwaggerEndpoint("/swagger/v1/swagger.json", "Challenger API V1");
+								})
+								.UseRouting()
+								.UseEndpoints(endPointRouteBuilder => endPointRouteBuilder.MapControllers())
+						)
+				)
+				.UseSerilog();
+	}
 }
