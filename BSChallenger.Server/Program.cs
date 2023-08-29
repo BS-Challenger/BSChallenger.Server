47d4cb3e3b798f3866d7bffbb2a50371d0420e2b
Futuremappermydud <54294576+Futuremappermydud@users.noreply.github.com>
2023-08-29 18:46:04 -0400

    fix: Quartz job (again)

--- BSChallenger.Server/Program.cs
+++ BSChallenger.Server/Program.cs
@@ -68,7 +68,7 @@ namespace BSChallenger.Server
 									q.AddTrigger(opts => opts
 										.ForJob(jobKey)
 										.WithIdentity("WeeklyScanHistoryJob-trigger")
-										.WithCronSchedule("0 0 * * 0"));
+										.WithCronSchedule("0 0 0 ? * SUN *"));
 								})
 								.AddQuartzHostedService(q => q.WaitForJobsToComplete = true)
                                 .AddControllers(options =>
