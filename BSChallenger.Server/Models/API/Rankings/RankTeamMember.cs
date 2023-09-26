2905d3602cbf5edf0962b063ddffdbaf4823a1d9
Futuremappermydud <54294576+Futuremappermydud@users.noreply.github.com>
2023-09-26 17:05:17 -0400

    forgot to ignore this lol (it crashed the server)

--- BSChallenger.Server/Models/API/Rankings/RankTeamMember.cs
+++ BSChallenger.Server/Models/API/Rankings/RankTeamMember.cs
@@ -1,5 +1,6 @@
 ﻿using BSChallenger.Server.Models.API.Users;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 namespace BSChallenger.Server.Models.API.Rankings
 {
@@ -8,8 +9,10 @@ namespace BSChallenger.Server.Models.API.Rankings
     {
         public int Id { get; set; }
         public int RankingId { get; set; }
+		[JsonIgnore]
 		public Ranking Ranking { get; set; }
         public int UserId { get; set; }
+        [JsonIgnore]
         public User User { get; set; }
         public RankTeamRole Role { get; set; }
 	}
