2f084abf2d2f7c9773f85fc7676ea7313db99727
Futuremappermydud <54294576+Futuremappermydud@users.noreply.github.com>
2023-12-13 20:04:19 -0500

    bruh

--- BSChallenger.Server/API/RankingsController.cs
+++ BSChallenger.Server/API/RankingsController.cs
@@ -1,45 +1,27 @@
 ﻿using BSChallenger.Server.Models;
-using BSChallenger.Server.Models.API.Maps;
 using BSChallenger.Server.Models.API.Rankings;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.Xml;
 
 namespace BSChallenger.Server.API
 {
     [ApiController]
-    [Route("/format/{map}")]
-    public class FormatController : ControllerBase
+    [Route("/rankings")]
+    public class RankingsController : ControllerBase
     {
         private readonly Database _database;
 
-        public FormatController(
+        public RankingsController(
             Database database)
         {
             _database = database;
         }
 
-        IEnumerable<string> GetFeatures(Map map)
-        {
-			foreach (var feature in map.Features)
-			{
-                //TODO, use formatter function + proper formatted name
-                yield return feature.Type + ": " + feature.Data;
-			}
-		}
-
         [HttpGet]
-        public ActionResult<string> Get([FromRoute] string map)
+        public ActionResult<IEnumerable<Ranking>> Get()
         {
-            var allMaps = _database.EagerLoadRankings().SelectMany(x => x.Levels).SelectMany(x => x.AvailableForPass);
-            var target = allMaps.FirstOrDefault(x => x.Identifier == map);
-
-			if (target != null)
-            {
-                return GetFeatures(target).Aggregate((x1, x2) => x1 + "\n" + x2);
-            }
-            return map;
+            return Ok(_database.EagerLoadRankings().Where(x => !x.Private));
         }
     }
 }
