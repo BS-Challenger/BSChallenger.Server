2a01687471b4e2de279e92c040aa85138a09716e
Futuremappermydud <54294576+Futuremappermydud@users.noreply.github.com>
2024-02-06 15:31:05 -0500

    Add Set invite command

--- BSChallenger.Server/Discord/Commands/Private/SetDiscordInvite.cs
+++ BSChallenger.Server/Discord/Commands/Private/SetDiscordInvite.cs
@@ -3,55 +3,46 @@ using BSChallenger.Server.Models;
 using BSChallenger.Server.Models.API.Rankings;
 using Discord.Interactions;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace BSChallenger.Server.Discord.Commands.Private
 {
-	public class SetOwner : InteractionModuleBase<SocketInteractionContext>
+	public class SetDiscordInvite : InteractionModuleBase<SocketInteractionContext>
 	{
 		private Database _database;
-		public SetOwner(Database database)
+		public SetDiscordInvite(Database database)
 		{
 			_database = database;
 		}
 
-		[SlashCommand("set-owner", "Sets the Owner of a ranking")]
-		public async Task Create([Autocomplete(typeof(RankingIdentifierAutoComplete))] string ranking, string userId)
+		static Regex InviteRegex = new Regex("(https?:\\/\\/|http?:\\/\\/)?(www.)?(discord.(gg|io|me|li)|discordapp.com\\/invite|discord.com\\/invite)\\/[^\\s\\/]+?(?=\\b)");
+
+		[SlashCommand("set-invite", "Sets the Discord Invite of a ranking")]
+		public async Task Create([Autocomplete(typeof(RankingIdentifierAutoComplete))] string ranking, string url)
 		{
 			if (Context.User.Id != 1191170302944759878)
 			{
 				await RespondAsync("Insufficient Permissions!", ephemeral: true);
 				return;
 			}
-			var user = _database.EagerLoadUsers().FirstOrDefault(x => x.DiscordId == userId);
-			if (user == null)
-			{
-				await RespondAsync("No BSChallenger account linked to this discord user!", ephemeral: true);
-				return;
-			}
+
 			var rankingObj = _database.EagerLoadRankings().FirstOrDefault(x => x.Identifier == ranking);
 			if (rankingObj == null)
 			{
 				await RespondAsync("Ranking not found!", ephemeral: true);
 				return;
 			}
-			var member = rankingObj?.RankTeamMembers?.FirstOrDefault(x => x.Role == RankTeamRole.Owner);
-			if (member == null)
+
+			if(InviteRegex.IsMatch(url))
 			{
-				var newOwner = new RankTeamMember()
-				{
-					Ranking = rankingObj,
-					User = user
-				};
-				user.AssignedRankings.Add(newOwner);
-				rankingObj.RankTeamMembers.Add(newOwner);
-				await _database.SaveChangesAsync();
+				await RespondAsync("Invite does not seem to be a valid discord invite! Setting anyways...");
 			}
-			else
-			{
-				member.User = user;
-				await _database.SaveChangesAsync();
-			}
+
+			rankingObj.DiscordURL = url;
+			await _database.SaveChangesAsync();
+
+			await RespondAsync($"Success! Discord invite set to <{url}>");
 		}
 	}
 }
