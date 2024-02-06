abe576d1572c6935d927ff40c7fd0bbd5237955e
Futuremappermydud <54294576+Futuremappermydud@users.noreply.github.com>
2024-02-06 15:52:47 -0500

    Add Discord URL Migration

---
+++ BSChallenger.Server/Migrations/20240206205046_discord_invite.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BSChallenger.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class discord_invite : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "discord_url",
+                table: "rankings",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "discord_url",
+                table: "rankings");
+        }
+    }
+}
